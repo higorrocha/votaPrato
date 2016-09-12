6324d764636bdee50a30f1a7276ab4703816b02b
higorrocha <higorrocha.14@gmail.com>
2016-09-12 10:52:57 -0300

    Adicionando validações básicas ao modelo restaurante

--- app/models/restaurante.rb
+++ app/models/restaurante.rb
@@ -1,2 +1,16 @@
 class Restaurante < ActiveRecord::Base
+	validates_presence_of :nome, message: "Deve ser preenchido"
+	validates_presence_of :endereco, message: "Deve ser preenchido"
+	validates_presence_of :especialidade, message: "Deve ser preenchido"
+
+	validates_uniqueness_of :nome, message: "Nome já cadastrado!"
+	validates_uniqueness_of :endereco, message: "Endereço já cadastrado!"
+
+	validate :primeira_letra_deve_ser_maiuscula
+
+	private
+	def primeira_letra_deve_ser_maiuscula
+	  errors.add(:nome,
+	      "A primeira letra deve ser maiúscula") unless nome =~ /[A-Z].*/
+	end
 end
